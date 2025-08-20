28f82fce2971b8ac756927e8677b5bf9abae880b
Bibi Khudaijatul Kubra <74587275+kimxael@users.noreply.github.com>
2025-08-20 23:49:53 +0530

    Add files via upload
    To create an array of size 10. Add squares of the numbers to this array.

---
+++ SqurArr.java
@@ -0,0 +1,19 @@
+class SqurArr{
+    public static void main(String[] args) {
+        int n[] = new int[10];   
+        n[0] = 1;    
+        n[1] = 4;    
+        n[2] = 9;    
+        n[3] = 16;   
+        n[4] = 25;   
+        n[5] = 36;  
+        n[6] = 49;  
+        n[7] = 64;   
+        n[8] = 81;   
+        n[9] = 100;  
+        System.out.println("Squares of numbers 1 to 10:");
+        for (int i = 0; i < 10; i++) {
+            System.out.println(n[i]);
+        }
+    }
+}
