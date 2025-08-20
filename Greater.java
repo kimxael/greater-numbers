74cd7e4a96013f6348ade705c6e41012a6e92a5c
Bibi Khudaijatul Kubra <74587275+kimxael@users.noreply.github.com>
2025-08-20 23:26:46 +0530

    Add files via upload

---
+++ Greater.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class Greater{
+    public static void main(String[]args){
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter two numbers a and b");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        if(a>b){
+            System.out.println(" a is greater");
+        }
+        else{System.out.println(" b is greater");}
+
+    }
+}
