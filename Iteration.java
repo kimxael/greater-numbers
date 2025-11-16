db3b92a52c6cce3531189e86635a32b005d27df5
Bibi Khudaijatul Kubra <74587275+kimxael@users.noreply.github.com>
2025-11-16 20:17:56 +0530

    Add files via upload

---
+++ Iteration.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+class Iteration {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter number: ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= 10; i++)
+            System.out.println(n + " x " + i + " = " + (n * i));
+
+        sc.close();
+    }
+}
