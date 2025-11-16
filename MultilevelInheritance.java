a121dde2df11a36c26f566437dc646e73ed52b4f
Bibi Khudaijatul Kubra <74587275+kimxael@users.noreply.github.com>
2025-11-16 22:25:44 +0530

    Add files via upload

---
+++ MultilevelInheritance.java
@@ -0,0 +1,24 @@
+public class MultilevelInheritance {
+    public static void main(String[] args) {
+        Puppy p = new Puppy();
+        p.eat(); 
+        p.bark(); 
+        p.weep(); 
+        }
+        }
+        class Animal {
+        void eat() {
+        System.out.println("Animals can eat");
+        }
+        }
+        class Dog extends Animal {
+        void bark() {
+        System.out.println("Dog can bark");
+        }
+        }
+        class Puppy extends Dog {
+        void weep() {
+        System.out.println("Puppy can weep");
+        }
+        }
+
